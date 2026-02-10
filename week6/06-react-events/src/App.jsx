61c54c64f4eb5cb3568699244e982969271b31d3
BHUVANESWARI BABU <info.bhuvaneswari@gmail.com>
2026-02-10 13:59:54 +0000

    06 react events

---
+++ week6/06-react-events/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button.jsx";
+import CoolerButton from "./components/CoolerButton.jsx";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Events in React</h1>
+      <Button />
+      <CoolerButton />
+    </div>
+  )
+}
+
