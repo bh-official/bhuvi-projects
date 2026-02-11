59f0e441871c167bcdc5cbb914a67752f15f5956
BHUVANESWARI BABU <info.bhuvaneswari@gmail.com>
2026-02-11 14:13:18 +0000

    09-conditional-rendering

--- week6/05-react-tailwind-styling/src/App.jsx
+++ week6/05-react-tailwind-styling/src/App.jsx
@@ -8,13 +8,27 @@ export default function App() {
       <Header />
       <main className="container mx-auto p-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <Books name="The Psychology of Money" author="Morgan Housel" img="https://minoa.com/cdn/shop/files/66_26de911f-e91f-4ef3-a5f8-54707df59802_1512x.jpg?v=1756464897" description="Timeless lessons on wealth, greed, and happiness"/>
-        <Books name="The Power of Letting Go" author="John Purkiss" img="https://hachette.imgix.net/books/9781783253630.jpg?auto=compress&w=440" description="How to let go and live the life you want"/>
-        <Books name="You Become What You Think" author="Shubham Kumar Singh" img="https://m.media-amazon.com/images/I/81WYPSP-ngL._SL1500_.jpg" description="Master your mind, Master your life"/>
+          <Books
+            name="The Psychology of Money"
+            author="Morgan Housel"
+            img="https://m.media-amazon.com/images/I/81gC3mdNi5L._SL1500_.jpg"
+            description="Timeless lessons on wealth, greed, and happiness"
+          />
+          <Books
+            name="The Power of Letting Go"
+            author="John Purkiss"
+            img="https://hachette.imgix.net/books/9781783253630.jpg?auto=compress&w=440"
+            description="How to let go and live the life you want"
+          />
+          <Books
+            name="You Become What You Think"
+            author="Shubham Kumar Singh"
+            img="https://m.media-amazon.com/images/I/81WYPSP-ngL._SL1500_.jpg"
+            description="Master your mind, Master your life"
+          />
         </div>
       </main>
-      <Footer/>
+      <Footer />
     </div>
-  
   );
 }
