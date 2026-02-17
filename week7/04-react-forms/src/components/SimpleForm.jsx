c8552383a7f7b2e60523548ba3084827276109e7
BHUVANESWARI BABU <info.bhuvaneswari@gmail.com>
2026-02-17 12:52:03 +0000

    forms variation with useactionState

--- week7/04-react-forms/src/components/SimpleForm.jsx
+++ week7/04-react-forms/src/components/SimpleForm.jsx
@@ -6,7 +6,7 @@ export default function SimpleForm() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("User submitted: ${username}, ${password}");
+    console.log(`User submitted: ${username}, ${password}`);
   }
   return (
     <div>
@@ -36,3 +36,78 @@ export default function SimpleForm() {
     </div>
   );
 }
+
+// adding useActionState version of the form
+
+// import { useState, useActionState } from "react";
+
+// export default function SimpleForm() {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   function action(prevState) {
+//     console.log(`User submitted: ${username}, ${password}`);
+//     return `Submitted: ${username}`;
+//   }
+
+//   const [message, formAction] = useActionState(action, "");
+
+//   return (
+//     <div>
+//       <form action={formAction}>
+//         <label htmlFor="username">Username:</label>
+//         <input
+//           type="text"
+//           id="username"
+//           value={username}
+//           onChange={(e) => setUsername(e.target.value)}
+//         />
+
+//         <label htmlFor="password">Password:</label>
+//         <input
+//           type="password"
+//           id="password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+
+//         <button type="submit">Submit</button>
+//       </form>
+
+//       <p>{message}</p>
+//     </div>
+//   );
+// }
+
+// replacing the above code with a simpler version that doesn't use useState, and instead relies on the formData passed to the action function by useActionState
+
+// import { useActionState } from "react";
+
+// export default function SimpleForm() {
+//   function handleSubmit(prevState, formData) {
+//     const username = formData.get("username");
+//     const password = formData.get("password");
+
+//     console.log(`User submitted: ${username}, ${password}`);
+
+//     return `Submitted: ${username}`;
+//   }
+
+//   const [message, formAction] = useActionState(handleSubmit, "");
+
+//   return (
+//     <div>
+//       <form action={formAction}>
+//         <label htmlFor="username">Username:</label>
+//         <input type="text" name="username" />
+
+//         <label htmlFor="password">Password:</label>
+//         <input type="password" name="password" />
+
+//         <button type="submit">Submit</button>
+//       </form>
+
+//       <p>{message}</p>
+//     </div>
+//   );
+// }
