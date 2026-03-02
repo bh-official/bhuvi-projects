f505d3873b6cfc4e99ec102bea39d6945be204a8
BHUVANESWARI BABU <info.bhuvaneswari@gmail.com>
2026-03-02 16:21:16 +0000

    01-intro-next.js

---
+++ week7/05-example-use-effect-timer/src/component/TimerDirty.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+export default function TimerDirty() {
+  console.log("TimerDirty component render");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("TimerDirty component useEffect");
+    setInterval(() => {
+      setCount((currentCount) => currentCount + 1);
+    }, 1000);
+  }, []);
+  return <div>Count: {count}</div>;
+}
